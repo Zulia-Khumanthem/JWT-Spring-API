287b8ae59682c7323176be94a2ed616d45082e40
Zulia Khumanthem <116053936+Zulia-Khumanthem@users.noreply.github.com>
2025-05-14 16:34:59 +0530

    Add files via upload

---
+++ spring-jwt-api-main/src/main/java/com/genspark/login/config/JwtService.java
@@ -0,0 +1,77 @@
+package com.genspark.login.config;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+@Service
+public class JwtService {
+
+    private static final String SECRET_KEY="d33dado5dbf3e0ffa079cc230fc4b95136b288b1bf7bd381c6d094587eb422b3find2ae45527e5c96004964f5621383a1c24de0e9e023a6e187339c6fact1eb1d8d634219a45baf1d973bfd1d04269477b2e26ab86cc7fc57fd371e57b19dcc71f814d458177dd712134b3d1bae0f19ad46d5ccd6cc360772f95539cec4d7085";
+
+    public String extractUserName(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver)
+    {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    public String generateToken(UserDetails userDetails)
+    {
+        return generateToken(new HashMap<>(), userDetails);
+    }
+
+    public String generateToken(Map<String, Object> extraClaims, UserDetails userDetails)
+    {
+        return Jwts
+                .builder()
+                .setClaims(extraClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    private Claims extractAllClaims(String token)
+    {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails)
+    {
+        final String userName = extractUserName(token);
+        return (userName.equals(userDetails.getUsername())) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+}
---
+++ spring-jwt-api-main/src/main/java/com/genspark/login/user/Role.java
@@ -0,0 +1,5 @@
+package com.genspark.login.user;
+
+public enum Role {
+    USER, ADMIN
+}
